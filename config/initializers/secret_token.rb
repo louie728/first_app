d21f43ef815f251af8675e798339d3d669846e13
louie728 <louie728@gmail.com>
2011-06-12 16:05:57 -0400

    Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TApp::Application.config.secret_token = '3a7f622ddc8d83c64fd706f416be078786a755569f6c87c53e0b7b8d6441a1e5236ffd07631a2070cbcf9fdd234eb1255f772cea3225df3459f60810381485e1'
